16a5a215b6527c138b3f0faaf70ee98384bf1008
Anton Heestand <anton.heestand@gmail.com>
2025-02-15 20:28:15 +0900

    Packages

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["GestureCanvas"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "1.8.1"),
+        .package(url: "https://github.com/heestand-xyz/CoreGraphicsExtensions", from: "2.0.1"),
     ],
     targets: [
         .target(
